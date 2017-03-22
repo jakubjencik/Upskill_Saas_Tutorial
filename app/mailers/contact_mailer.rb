fd5bbb49796ec69ad67a13a2a9efb7c1db168ea4
Jakub Jencik <jakub.jencik@gmail.com>
2017-03-22 15:06:19 +0000

    added missing line of code in ContactMailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
    default to: 'jakub.jencik@gmail.com' 
    
-   def contact_email
+   def contact_email(name, email, body)
    @name = name
    @email = email
    @body = body
